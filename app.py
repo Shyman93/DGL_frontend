a36450efe6025f2565651a235a5a6cc2aa60f859
Shayan Moghaddam <shayanm93@gmail.com>
2023-09-19 20:59:09 +0100

    removed unused widgets from map

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import folium
 import streamlit as st
+import folium
 from folium.plugins import Draw
-
 from streamlit_folium import st_folium
 
 # Set the title of your Streamlit app
@@ -14,23 +13,55 @@ st.write(
     "the satellite image you'd like to analyze."
 )
 
-# Create a Folium map
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=10)
 
-# Add the Draw control to the map
-Draw(export=True).add_to(m)
+def create_map(center):
+    m = folium.Map(
+        location=center,
+        zoom_start=12,
+        control_scale=True,
+        tiles="OpenStreetMap",
+        attr='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    )
+    Draw(
+        export=False,
+        position="topleft",
+        draw_options={
+            "polyline": False,
+            "poly": False,
+            "circle": False,
+            "polygon": False,
+            "marker": False,
+            "circlemarker": False,
+            "rectangle": True,
+        }).add_to(m)
+    return m
 
-# Display the map in Streamlit
+st.title("Folium Map with Rectangle")
+
 c1, c2 = st.columns(2)
+
+initial_center = [51.5328, -0.0769]
 with c1:
-    output = st_folium(m, width=700, height=500)
+    output = st_folium(create_map(initial_center))
 
 with c2:
-    st.write("Map Output")
+    info = output.get("all_drawings")
+    if info:
+        coordinates = info[0]["geometry"]["coordinates"][0]
+
+        A1 = coordinates[0]
+        A2 = coordinates[1]
+        B1 = coordinates[2]
+        B2 = coordinates[3]
+
+        st.write("Coordinates of the rectangle")
+
+        st.write(f"Top Left: {A1}")
+        st.write(f"Top Right: {A2}")
+        st.write(f"Bottom Left: {B1}")
+        st.write(f"Bottom Right: {B2}")
 
 
-    lst = [output["bounds"]["_southWest"]["lat"], output["bounds"]["_southWest"]["lng"], output["bounds"]["_northEast"]["lat"], output["bounds"]["_northEast"]["lng"]]
-    st.write(lst)
 # Add a date input for satellite image analysis
 selected_date = st.date_input("Select a date for satellite image analysis")
 
